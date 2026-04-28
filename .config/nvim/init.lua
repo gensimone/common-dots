5916d4c936fd05c8dc39a5b3ed5dcdf3b50c270c
Simone Gentili <gensimone.git@gmail.com>
2026-04-28 15:39:17 +0200

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -98,7 +98,7 @@ require("cmp").setup({
         ["<C-e>"] = cmp.mapping.abort(),
         ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
     }),
-    sources = cmp.config.sources({{ name = "nvim_lsp" }})
+    sources = cmp.config.sources({{ name = "nvim_lsp" }, { name = "path" }})
 })
 
 require("transparent").setup({
